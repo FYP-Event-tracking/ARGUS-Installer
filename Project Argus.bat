8bdf5aec51b319036a5ab4a0974b0d38f08876a6
Manuja Dewmina <manuja1bc@gmail.com>
2024-07-12 00:56:05 +0530

    Version 1.0 Complete

--- Project Argus.bat
+++ Project Argus.bat
@@ -4,3 +4,5 @@ start "" "Frontend/ProjectArgus.jar"
 start "" "Track and Count System/RestApi/TCS_RestApi.bat"
 
 start "" "Track and Count System/WebSocket/TCS_WebSocket.bat"
+
+start "" "Track and Count System/RestApi_continue/TCS_RestApi_continue.bat"
